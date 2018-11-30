40c2f0919cbced5a9b27b34a591f98315c04882c
mpabarca <mpabarca@uc.cl>
2018-11-30 09:32:02 -0300

    Viernes 30/Noviembre/2018 Primer acercamiento a la app con esqueleto del index.html / index.js Mientras que para cipher.js aun se ven los detalles para su correcto funcionamiento

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,32 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset,password) => {
+    let ascii;
+    let result="";
+    //let letra="";
+    for (let i=0; i<password.length;i++){
+      console.log(password.chartCodeAt(i));
+      ascii=password.chartCodeAt(i);
+      result+= String.fromCharCode(ascii + offset);
+      //let nuevo=(ascii - 65 + offset)%26 + 65;
+      //nuevo= función fórmula para nuevo asccii con el desplazamiento de michelle
+      //en nuevo usar offset entregado
+      //letra=nuevo.fromCharCodeAt();
+      //result+=letra
+    }
+    return result
+
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (offset,password) => {
+    let ascii;
+    let result_decodificar="";
+    for (var i=0; i<result.length;i++){
+      
+      //let letra="";
+      result_decodificar += String.fromCharCode(ascii - offset);
+    }
+    return result_decodificar
+
   }
+  return result_decodificar
+  
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-/* Acá va tu código */
+
+//window onload carga primero todo el DOM 
+/* Acá va tu código 
+DOM JS interactúa directamente con el html
+Document Object Model*/
+
+//window.cipher;
+//let password = document.getElementById("cajaInput").value;
+// document.getElementById es para llamar a la caja del html
+// mientras que el .value toma el valor que el usuario le da en html y se ocupa en el JS
+//let offset = document.getElementById("cajaOffset").value
+
+//id=presentacion
+function obtenerExplicacion() {
+    
+    let div = document.getElementById("presentacion");
+    div.style.display = "none";
+    let div2 = document.getElementById("explicacion");
+    div2.style.display = "block";
+    let div3 = document.getElementById("cifrado");
+    div3.style.display = "none";
+}
+
+//id=explicacion
+function volverInicio(){
+
+    let div = document.getElementById("presentacion");
+    div.style.display = "block";
+    let div2 = document.getElementById("explicacion");
+    div2.style.display = "none";
+    let div3 = document.getElementById("cifrado");
+    div3.style.display = "none";
+
+}
+
+//id=cifrado
+function comenzarApp(){
+    let div = document.getElementById("presentacion");
+    div.style.display = "none";
+    let div2 = document.getElementById("explicacion");
+    div2.style.display = "none";
+    let div3 = document.getElementById("cifrado");
+    div3.style.display = "block";  
+}
+
+
+
+function cifrar() {
+    let contraseña=document.getElementById("pass").value;
+    let desplazamiento=document.getElementById("desp").value;
+    cipher.encode(desplazamiento,contraseña);
+}
+
+function descifrar() {
+    let contraseña=document.getElementById("pass").value;
+    let desplazamiento=document.getElementById("desp").value;
+    cipher.decode(desplazamiento,contraseña);
+}
